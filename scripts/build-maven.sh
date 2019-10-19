f09f5713f4736e09b6f738056faf260cfa459b10
Shiksha Malhotra <shikshamalhotra@outlook.com>
2019-10-19 19:45:19 +0200

    updated Jar path

--- scripts/build-maven.sh
+++ scripts/build-maven.sh
@@ -7,4 +7,4 @@ NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 
 #mv  target/${NAME}-${VERSION}.jar target/entrypoint.jar
-mv /root/.m2/repository/org/springframework/samples/spring-petclinic/2.1.0.BUILD-SNAPSHOT/spring-petclinic-2.1.0.BUILD-SNAPSHOT.jar target/entrypoint.jar
+mv /root/.m2/repository/net/notejam/spring/0.0.1-SNAPSHOT/spring-0.0.1-SNAPSHOT.jar target/entrypoint.jar
